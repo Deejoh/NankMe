a57b8606cdfd6d0717126d054a791eb4d92765f5
Deejoh <82794466+Deejoh@users.noreply.github.com>
2021-04-19 14:49:18 +1000

    Update images.js

--- images.js
+++ images.js
@@ -1,6 +1,11 @@
 
 const image_array = [
-    './images/1.jpeg',
-    './images/2.jpeg',
-    './images/3.jpg'
+    './images/1.jpg',
+    './images/2.jpg',
+    './images/3.jpg',
+    './images/4.jpg',
+    './images/5.jpg',
+    './images/6.jpg',
+    './images/7.jpg',
+    './images/8.jpg'
 ];
